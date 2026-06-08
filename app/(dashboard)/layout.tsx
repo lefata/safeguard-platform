8efd9a82ade7c08007ac9bd4437cdf3900403ebb
lefata <franklefata@gmail.com>
2026-06-08 12:53:57 +0100

    Delete app/(dashboard) directory

--- app/(dashboard)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardLayout } from "@/components/layouts/dashboard-layout";
-
-export default function DashboardGroupLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
