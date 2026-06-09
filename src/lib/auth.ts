c0581d7ac6d9202de00abaf51ae991b70a17e41b
lefata <franklefata@gmail.com>
2026-06-09 09:48:56 +0100

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -50,7 +50,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     AzureADProvider({
       clientId: process.env.AZURE_AD_CLIENT_ID!,
       clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
-      tenantId: process.env.AZURE_AD_TENANT_ID!,
+      issuer: `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/v2.0`,
     }),
     CredentialsProvider({
       name: 'credentials',
