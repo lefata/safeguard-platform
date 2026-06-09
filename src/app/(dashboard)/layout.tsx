3714ab4d54d7b191c063708840e44c4723ff593c
lefata <franklefata@gmail.com>
2026-06-09 11:23:18 +0100

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,7 @@
+// src/app/(dashboard)/layout.tsx
 import { DashboardLayout } from "@/components/layouts/dashboard-layout";
+
+export const dynamic = 'force-dynamic';
 
 export default function DashboardGroupLayout({
   children,
