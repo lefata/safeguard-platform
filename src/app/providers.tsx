3ba6e4a3a7180cbdcbb80b2b908fa167b2e200c1
lefata <franklefata@gmail.com>
2026-06-11 14:23:42 +0100

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,15 @@
-// src/app/providers.tsx
-"use client";
+'use client';
 
-import { SessionProvider } from "next-auth/react";
+import { ReactNode } from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ThemeProvider } from '@/lib/theme-provider';
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem>
+      <ThemeProvider>
+        {children}
+      </ThemeProvider>
+    </NextThemesProvider>
+  );
 }
